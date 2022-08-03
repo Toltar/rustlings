b86b51a49890dffd72bc3c4072620a59c82203e8
Toltar <matthew@matthew-dickens.com>
2022-08-02 17:34:20 -0700

    Finished the 37 exercises

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -1,16 +1,18 @@
 // enums1.rs
 // No hints this time! ;)
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    Quit(String),
+    Echo(String),
+    Move(String),
+    ChangeColor(String),
 }
 
 fn main() {
-    println!("{:?}", Message::Quit);
-    println!("{:?}", Message::Echo);
-    println!("{:?}", Message::Move);
-    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::Quit(String::from("Do you want to quit?")));
+    println!("{:?}", Message::Echo(String::from("ECHO! ECHO!")));
+    println!("{:?}", Message::Move(String::from("Moving")));
+    println!("{:?}", Message::ChangeColor(String::from("Changing color")));
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -8,11 +8,11 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(numberOfApples: u32) -> u32 {
+    let price = if numberOfApples > 40 { 1 } else { 2 };
+    price * numberOfApples
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
